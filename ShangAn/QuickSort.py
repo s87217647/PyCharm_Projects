84ca9a000cab20fc7f0ad0931290d7658bd756a2
Swift <s87217647@gmail>
2021-08-07 12:07:15 -0700

    对象双指针QuickSort

--- ShangAn/QuickSort.py
+++ ShangAn/QuickSort.py
@@ -1,23 +1,46 @@
-class QuickSort:
-    # I like 1th and 4th modelty
+# 同向双指针is not very intuitive，trying another way to solve the prob
+# Note: next time, spend time on choosing a intuitive way to remember
+# Todo: Write the code and debug it yourself 对向双指针
+# And, 捷抄的答案，after all
 
+class QuickSort:
     def sort(self, A):
         self.QuickSort(A, 0, len(A) - 1)
 
-    # Pivot: All on the left are smaller than to the right6
+    # Pivot: All on the left are smaller than to the right
 
     def QuickSort(self, A, start, end):
+        if start >= end:
+            return
+
         mid = self.partition(A, start, end)
         self.QuickSort(A, start, mid - 1)
         self.QuickSort(A, mid + 1, end)
 
-        return
 
-    # Never saw this partition in any resources
     def partition(self, A, start, end):
-        m, pivot = start, A[start]
+        pivot = A[start]
+        i, j = start + 1, end
+        while i <= j:
+            while i <= j and A[i] <= pivot:
+                i += 1
 
-        return 0
+            while i <= j and A[j] > pivot:
+                j -= 1
+
+            if i > j:
+                break
+
+            self.swap(A, i, j)
+            i += 1
+            j -= 1
+
+        A[start] = A[j]
+        A[j] = pivot
+        return j
+
+
+
 
     def swap(self, A, i, j):
         temp = A[i]
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from MergeSort1 import MergeSort1
-
+from QuickSort import QuickSort
+from QuickSort2 import QuickSort2
+import random
 if __name__ == '__main__':
-    m = MergeSort1()
-    A = [5, 8, 4, 6, 1, 9]
 
-    print(m.sort(A))
 
-    print("This is the end, hotld your breath and count to ten")
+    nums = []
+    qs = QuickSort2()
+    for _ in range(10):
+        nums.append(random.randint(0, 100))
+
+    print(nums)
+    qs.sort(nums)
+    print(nums)
+
+
+    print("This is the end, hold your breath and count to ten")
